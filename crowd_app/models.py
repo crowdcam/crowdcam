0fd7c7a2187ad5bf522980f5fcb91658b34cd503
John Salata <jsalata90@gmail.com>
2025-04-01 19:08:19 +0000

    Created generic media model

--- crowd_app/models.py
+++ crowd_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import os
 
 # Create your models here.
+class Media(models.Model):
+    
+    # TODO: when we get media storage going, set this up
+    # media_path = models.FilePathField()
+    media_path = models.CharField(max_length=200)
+    user_id = models.IntegerField()
+    organization_id = models.IntegerField()
+    status = models.BooleanField(default=False)
+    created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.media_path
