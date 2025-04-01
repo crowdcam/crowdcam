78add0a03f4368df8d21ec659fced0b11638c14e
scasada38 <s.casada38@gmail.com>
2025-04-01 20:31:04 +0000

    Added page templating to index page

--- crowd_app/views.py
+++ crowd_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return render(request, "crowd_app/index.html", None)
+    ##template = loader.get_template("index.html")
+    return (render(request, "crowd_app/index.html"))
