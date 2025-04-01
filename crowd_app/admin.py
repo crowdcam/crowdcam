6f27fea71d0cb376b63d3ee1627d3b5f77ecbe0c
John Salata <jsalata90@gmail.com>
2025-04-01 19:10:03 +0000

    Added comments

--- crowd_app/admin.py
+++ crowd_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Media
 
-# Register your models here.
+# Every model must be registered in the admin.py
+# file in order for it to be usable on the admin (dev) dashboard
 admin.site.register(Media)
