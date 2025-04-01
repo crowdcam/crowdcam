4d85c3e73386e8931c29d9ac8db0df62b628ec7e
John Salata <jsalata90@gmail.com>
2025-04-01 19:37:47 +0000

    Created all media and media by id viewing page

--- crowd_app/views.py
+++ crowd_app/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Media
+
 
 # Create your views here.
 def index(request):
     return render(request, "crowd_app/index.html", None)
+
+def media_index(request):
+    media_list = Media.objects.order_by("-created")
+    context = {"media_list": media_list}
+    return render(request, "crowd_app/media/index.html", context)
+
+def media_view(request, media_id):
+    media = Media.objects.get(id=media_id)
+    context = {"media": media}
+    return render(request, "crowd_app/media/view.html", context)
