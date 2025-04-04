7d8e9c6a0ecd51ffceb42ab5eeb026c8e110c8cb
sheepman39 <jrsalata@iu.edu>
2025-04-04 02:49:37 +0000

    Created login page

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from django.contrib.auth.views import LoginView
 from . import views
 
+app_name = "users"
+
 urlpatterns = [
     path("create_user/", views.register_user, name="create_user"),
+    path("login/", views.user_login, name="login"),
+    
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 app_name = "users"
 
@@ -8,10 +9,29 @@ def register_user(request):
     if(request.method == "POST"):
         form = UserCreationForm(request.POST)
         if(form.is_valid()):
-            form.save()
+
+            # save the user and get the new created user
+            new_user = form.save()
+
+            # login as new user
+            login(request, new_user)
+            
             # redirect to app_name:name_of_page
             return redirect("crowd_app:media_index")
+        
     else: 
         form = UserCreationForm()
     context = {"form": form}
     return render(request, "users/register.html", context)
+
+def user_login(request):
+    
+    if(request.method == "POST"):
+        form = AuthenticationForm(data=request.POST)
+        if(form.is_valid()):
+            login(request, form.get_user())
+            return redirect("crowd_app:media_index")
+    else:
+        form = AuthenticationForm()
+    context = {"form": form}
+    return render(request, "users/login.html", context)
