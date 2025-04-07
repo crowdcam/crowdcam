29206ab6dd9bd757813e8eaafab15a6aa3abe301
Ubuntu <azureuser@CrowdCam.cowthp54g5fe5f2yli0erylpmb.bx.internal.cloudapp.net>
2025-04-07 17:14:27 +0000

    reorder installed apps

--- crowdcam_server/settings.py
+++ crowdcam_server/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "mod_wsgi.server",
     "crowd_app",
     "crowdcam_server",
+    "mod_wsgi.server",
+
 ]
 
 MIDDLEWARE = [
