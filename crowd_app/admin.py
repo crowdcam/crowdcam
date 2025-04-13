b4314469b24c8928e5d31cfc9292c4e654688b83
sheepman39 <jrsalata@iu.edu>
2025-04-13 02:11:53 +0000

    Added new models to admin for editing

--- crowd_app/admin.py
+++ crowd_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Media
+from .models import Media, Organization, Tag
 
 # Every model must be registered in the admin.py
 # file in order for it to be usable on the admin (dev) dashboard
 admin.site.register(Media)
+admin.site.register(Organization)
+admin.site.register(Tag)
