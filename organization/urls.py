0739a1e0cfbd1cc0960d2a712351ca95557998e2
sheepman39 <jrsalata@iu.edu>
2025-04-27 00:02:24 +0000

    Refactored some of org admin pages

--- organization/urls.py
+++ organization/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from . import views_admin
 
 app_name = "organization"
 
@@ -8,10 +9,11 @@ urlpatterns = [
     path("join_org/", views.join_org, name="join_org"),
     path("<int:org_id>", views.org_view, name="org_view"),
     path("", views.user_orgs, name="user_orgs"),
-    path("<int:org_id>/admin", views.admin_index, name="admin_index"),
-    path("<int:org_id>/admin/download", views.download_users, name="download_users"),
-    path("<int:org_id>/admin/users", views.manage_users, name="manage_users"),
-    path("<int:org_id>/admin/users/<int:user_id>", views.manage_user, name="manage_user"),
-    path("<int:org_id>/admin/join_code", views.set_join_code, name="set_join_code")
+    path("<int:org_id>/admin", views_admin.admin_index, name="admin_index"),
+    path("<int:org_id>/admin/download", views_admin.download_users, name="download_users"),
+    path("<int:org_id>/admin/users", views_admin.manage_users, name="manage_users"),
+    path("<int:org_id>/admin/users/<int:user_id>", views_admin.manage_user, name="manage_user"),
+    path("<int:org_id>/admin/join_code", views_admin.set_join_code, name="set_join_code")
+    
     
 ]
