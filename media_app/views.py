626f6def013da0843fd36ebf4354494760069596
sheepman39 <jsalata90@gmail.com>
2025-04-29 00:34:33 +0000

    Created user media page

--- media_app/views.py
+++ media_app/views.py
@@ -83,13 +83,13 @@ def delete_media(request, org_slug, media_id):
 def user_media(request, filter=None):
     
     if(filter is None):
-        media_list = Media.objects.filter(user=request.user).filter(status=None).order_by("-created")
+        media_list = Media.objects.filter(user=request.user).filter(status=None).order_by("created")
     elif(filter == "approved"):
-        media_list = Media.objects.filter(user=request.user).filter(status=True).order_by("-created")
+        media_list = Media.objects.filter(user=request.user).filter(status=True).order_by("created")
     elif(filter == "all"):
-        media_list = Media.objects.filter(user=request.user).order_by("-created")
+        media_list = Media.objects.filter(user=request.user).order_by("created")
     else:
-        media_list = Media.objects.filter(ouser=request.user).filter(status=False).order_by("-created")
+        media_list = Media.objects.filter(user=request.user).filter(status=False).order_by("created")
 
     context = {"media_list": media_list, "filter": filter}
     return render(request, "media_app/user_media.html", context)
