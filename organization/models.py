2003ccdeebfb970a3f076aee708c0558ae9b248b
John Salata <51085651+sheepman39@users.noreply.github.com>
2025-04-29 07:35:32 -0500

    Revert "created test cases"

--- organization/models.py
+++ organization/models.py
@@ -1,25 +1,21 @@
 from django.db import models
-from django.utils.text import slugify
+from django.template.defaultfilters import slugify
 
+# Create your models here.
 class Organization(models.Model):
+    #TODO validate field length
     name = models.CharField(max_length=255)
     join_code = models.CharField(max_length=20, null=True)
     accepting_users = models.BooleanField(default=False)
     slug = models.SlugField(max_length=255, unique=True, blank=True)
 
+
     class Meta:
-        app_label = 'organization'
         permissions = (
             ("user", "Member of org"),
             ("mod", "Moderator of org"),
             ("admin", "Admin of org")
         )
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super().save(*args, **kwargs)
-        
     def __str__(self):
         return self.name
 
@@ -31,8 +27,9 @@ class Organization(models.Model):
 
     def get_admin_group(self):
         return self.name + "_admin"
-
-class MediaFile(models.Model):
-    organization = models.ForeignKey('Organization', on_delete=models.CASCADE, related_name='media_files')
-    file = models.FileField(upload_to='organization_media/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
+    
+    # redefine save() because we need to make a slug
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
