f2f5dfcedc5bd28b92b5b2c93f3839590cb6c9a0
scasada38 <s.casada38@gmail.com>
2025-05-03 03:37:18 +0000

    Fixed misc bugs. Added Media testcase

--- organization/forms.py
+++ organization/forms.py
@@ -1,10 +1,19 @@
 from django import forms
 from .models import Organization
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import ValidationError
 
 class OrganizationForm(forms.ModelForm):
     class Meta:
         model = Organization
         fields = ["name"]
+
+    def clean_name(self):
+        name = self.cleaned_data.get("name")
+        if Organization.objects.filter(name=name).exists():
+            # check to make sure name is unique
+            raise ValidationError(f"The name '{name}' is already taken.")
+        return name
 
 class JoinCodeForm(forms.ModelForm):
     class Meta:
